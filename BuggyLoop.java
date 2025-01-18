1b0aa7e614b892a0a947437ab43f51dd98b849d8
BugHunter-X <askzenai@gmail.com>
2025-01-18 11:44:05 +0100

    Add BuggyLoop.java file

---
+++ BuggyLoop.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+            if (i == 5) {
+                break; // Exit the loop when i is 5
+            }
+        }
+        System.out.println("Loop finished");
+    }
+}
